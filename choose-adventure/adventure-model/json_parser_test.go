0e0e72a5946aa779cd898759f74eabf2c1d7bde2
luxgriffith <griffith.scg@gmail.com>
2024-02-12 19:50:42 -0700

    Fix weird git problems caused by misclick in vscode

--- choose-adventure/adventure-model/json_parser_test.go
+++ choose-adventure/adventure-model/json_parser_test.go
@@ -81,82 +81,6 @@ func setupTestStory() {
 	}
 }
 
-var ExpectedStory *Story
-
-func setupTestStory() {
-	ExpectedStory = &Story{
-		arcs: map[string]*Arc{
-			"intro": {
-				text: []string{
-					"Intro to story",
-					"a paragraph",
-					"another paragraph",
-					"weeeeeeee"},
-				options: []*Option{
-					{
-						text:     "Option A",
-						arcTitle: "A",
-					},
-					{
-						text:     "Option B",
-						arcTitle: "B",
-					},
-					{
-						text:     "Option C",
-						arcTitle: "C",
-					},
-				},
-			},
-			"A": {
-				text: []string{
-					"Option A's Story",
-					"a paragraph",
-					"another paragraph",
-					"weeeeeeee",
-					"your stuck teehee",
-				},
-				options: []*Option{
-					{
-						text:     "Option B",
-						arcTitle: "B",
-					},
-				},
-			},
-			"B": {
-				text: []string{
-					"Option B's Story",
-					"a paragraph",
-					"another paragraph",
-					"weeeeeeee",
-					"jail time",
-				},
-				options: []*Option{
-					{
-						text:     "Option A",
-						arcTitle: "A",
-					},
-				},
-			},
-			"C": {
-				text: []string{},
-				options: []*Option{
-					{
-						text:     "Option D",
-						arcTitle: "D",
-					},
-				},
-			},
-			"D": {
-				text: []string{
-					"YOU DIED",
-					"insert dark souls noise",
-				},
-				options: []*Option{},
-			},
-		},
-	}
-}
-
 func TestBadPath(t *testing.T) {
 	_, err := readStory("lolololol")
 	if err == nil {
@@ -172,22 +96,7 @@ func TestBadFormat(t *testing.T) {
 		t.Fail()
 		t.Fatalf("TestBadPath Failed, err was nil")
 	}
-	t.Logf("TestBadPath triggered error: %v", err.Error())
-	_, err := readStory("lolololol")
-	if err == nil {
-		t.Fail()
-		t.Fatalf("TestBadPath Failed, err was nil")
-	}
-	t.Logf("TestBadPath triggered error: %v", err.Error())
-}
-
-func TestBadFormat(t *testing.T) {
-	_, err := readStory("../badformatstory.json")
-	if err == nil {
-		t.Fail()
-		t.Fatalf("TestBadPath Failed, err was nil")
-	}
-	t.Logf("TestBadPath triggered error: %v", err.Error())
+	t.Logf("TestBadFormat triggered error: %v", err.Error())
 }
 
 func TestReadableStory(t *testing.T) {
