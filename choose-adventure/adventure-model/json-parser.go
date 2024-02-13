4ce2cf59d890c4b59a65216652e41ec8794c32f8
luxgriffith <griffith.scg@gmail.com>
2024-02-12 20:15:14 -0700

    Move story object definitions to their own file and add some helpful helper methods

--- choose-adventure/adventure-model/json-parser.go
+++ choose-adventure/adventure-model/json-parser.go
@@ -1,22 +1,5 @@
 package adventure_model
 
-// The Object that defines the story
-type Story struct {
-	arcs map[string]*Arc
-}
-
-// The object that defines an option in an arc
-type Option struct {
-	text     string
-	arcTitle string
-}
-
-// The Object that defines a specific arc
-type Arc struct {
-	text    []string
-	options []*Option
-}
-
 // Method that takes in a file path, and returns a Story object and nil on success or nil and an error on failure
 func readStory(path string) (*Story, error) {
 	return nil, nil
---
+++ choose-adventure/adventure-model/story.go
@@ -0,0 +1,62 @@
+package adventure_model
+
+// The Object that defines the story
+type Story struct {
+	arcs map[string]*Arc
+}
+
+// Return a string that represents the contents of the story for testing and debugging purposes
+func (s *Story) toString() string {
+	out := ""
+	for idx, arcTitle := range s.getArcTitles() {
+		out = out + arcTitle + ":" + s.arcs[arcTitle].toString()
+		if idx == len(s.getArcTitles())-1 {
+			out += "\n"
+		}
+	}
+	return out
+}
+
+// Returns a list of the story's arc titles
+func (s *Story) getArcTitles() []string {
+	arcs := make([]string, 0, len(s.arcs))
+	for arcTitle := range s.arcs {
+		arcs = append(arcs, arcTitle)
+	}
+	return arcs
+}
+
+// The Object that defines a specific arc
+type Arc struct {
+	text    []string
+	options []*Option
+}
+
+// Return a string that represents the contents of the arc for testing and debugging purposes
+func (a *Arc) toString() string {
+	out := ""
+	for idx, paragraph := range a.text {
+		out += paragraph
+		if idx != len(a.text)-1 {
+			out += ", "
+		}
+	}
+	for idx, option := range a.options {
+		out += option.toString()
+		if idx != len(a.options)-1 {
+			out += ", "
+		}
+	}
+	return out
+}
+
+// The object that defines an option in an arc
+type Option struct {
+	text     string
+	arcTitle string
+}
+
+// Return a string that represents the contents of the Option for testing and debugging purposes
+func (o *Option) toString() string {
+	return o.text + ", " + o.arcTitle
+}
