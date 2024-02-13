e2925e38b1e8d1d10852bd3828bfd52819ed8181
luxgriffith <griffith.scg@gmail.com>
2024-02-12 23:19:06 -0700

    Implement getnextarc

--- choose-adventure/adventure-model/server-backend.go
+++ choose-adventure/adventure-model/server-backend.go
@@ -1,5 +1,10 @@
 package adventure_model
 
+import (
+	"errors"
+	"fmt"
+)
+
 // Defines the thread that runs the actual http server
 func runServer() {
 
@@ -7,5 +12,10 @@ func runServer() {
 
 // Takes in an option the user picked and the story, and returns the arc that option leads to and its title, as well as an optional error
 func getNextArc(option *Option, story *Story) (title string, arc *Arc, err error) {
-	return "", nil, nil
+	if option == nil || story == nil {
+		return "", nil, errors.New(fmt.Sprintf("Recieved nil input"))
+	}
+	nextArcTitle := option.arcTitle
+	nextArc := story.arcs[nextArcTitle]
+	return nextArcTitle, nextArc, nil
 }
