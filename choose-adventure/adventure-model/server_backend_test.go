cc65b11b7fb2b34a6d0ffbf8acd0a026a948cbad
luxgriffith <griffith.scg@gmail.com>
2024-02-12 20:19:09 -0700

    Add  tests for server backend

--- choose-adventure/adventure-model/server_backend_test.go
+++ choose-adventure/adventure-model/server_backend_test.go
@@ -5,9 +5,35 @@ import (
 )
 
 func TestGetNextArc(t *testing.T) {
-
+	setupTestStory()
+	expectedTitle := "A"
+	expectedArc := ExpectedStory.arcs[expectedTitle]
+	inputOption := &Option{
+		text:     "Option A",
+		arcTitle: "A",
+	}
+	inputStory, _ := readStory("../story.json")
+	resultTitle, resultArc, err := getNextArc(inputOption, inputStory)
+	if err != nil {
+		t.Fail()
+		t.Fatalf("TestGetNextArc Failed: Error triggered, %v", err.Error())
+	}
+	if expectedTitle != resultTitle {
+		t.Fail()
+		t.Fatalf("TestGetNextArc Failed, title mismatch: Expected %v, got %v", expectedTitle, resultTitle)
+	}
+	if expectedArc.toString() != resultArc.toString() {
+		t.Fail()
+		t.Fatalf("TestGetNextArc Failed, arc mismatch: expected %v, got %v", expectedArc.toString(), resultArc.toString())
+	}
+	t.Logf("TestGetNextArc Passed")
 }
 
 func TestBadInputs(t *testing.T) {
-
+	_, _, err := getNextArc(nil, nil)
+	if err == nil {
+		t.Fail()
+		t.Fatalf("TestBadInputs Failed, no error returned")
+	}
+	t.Logf("TestBadInputs caused error %v", err.Error())
 }
