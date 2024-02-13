bcc6751aa89addb44957bace2f9453af1eb78600
luxgriffith <griffith.scg@gmail.com>
2024-02-12 21:03:50 -0700

    Implement first pass of readStory

--- choose-adventure/adventure-model/json-parser.go
+++ choose-adventure/adventure-model/json-parser.go
@@ -1,6 +1,23 @@
 package adventure_model
 
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
 // Method that takes in a file path, and returns a Story object and nil on success or nil and an error on failure
 func readStory(path string) (*Story, error) {
-	return nil, nil
+	fileData, err := os.ReadFile(path)
+	if err != nil {
+		fmt.Printf("Error reading file: %v\n", err.Error())
+		return nil, err
+	}
+	var story *Story
+	err = json.Unmarshal(fileData, &story)
+	if err != nil {
+		fmt.Printf("Error while unmarshalling json: %v\n", err.Error())
+		return nil, err
+	}
+	return story, nil
 }
